da7080899a12012905085a091fc78b8029e5449d
22babar <babar.pchs@gmail.com>
2026-05-02 14:15:28 +0500

    add env variable

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
+import env from 'dotenv';
+
+env.config();
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'social',
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 db.getConnection((err) => {
